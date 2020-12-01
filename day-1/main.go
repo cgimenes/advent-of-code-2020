5b62cb72bf665c949b60ab598051686a3a283e71
Marcelo Gimenes de Oliveira <marcelo.gimenes.oliveira@gmail.com>
2020-12-01 16:51:15 -0300

    Day 1

---
+++ day-1/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	path, _ := filepath.Abs("day-1/input")
+	input, err := ioutil.ReadFile(path)
+	check(err)
+	lines := strings.Split(string(input), "\n")
+
+	fmt.Printf("Part 1: %d\n", part1(lines))
+	fmt.Printf("Part 2: %d\n", part2(lines))
+}
+
+func part1(lines []string) int {
+	for i := 0; i < (len(lines) - 1); i++ {
+		x, _ := strconv.Atoi(lines[i])
+		for j := i + 1; j < len(lines); j++ {
+			y, _ := strconv.Atoi(lines[j])
+			if x + y == 2020 {
+				return x * y
+			}
+		}
+	}
+	return 0
+}
+
+func part2(lines []string) int {
+	for i := 0; i < (len(lines) - 2); i++ {
+		x, _ := strconv.Atoi(lines[i])
+		for j := i + 1; j < (len(lines) - 1); j++ {
+			y, _ := strconv.Atoi(lines[j])
+			for k := j + 1; k < len(lines); k++ {
+				z, _ := strconv.Atoi(lines[k])
+				if x + y + z == 2020 {
+					return x * y * z
+				}
+			}
+		}
+	}
+	return 0
+}
